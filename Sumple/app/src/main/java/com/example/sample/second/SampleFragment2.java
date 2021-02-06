20c8cf9bf0adb7b6fce3bb69d922a7a2ff71542f
saku <net.mak777@gmail.com>
2021-02-06 16:35:13 +0900

    OnClickListenerイベントを使用した学習。
    　・Buttonコントロール押下回数のカウント
    　・3の倍数でナベアツ表示

--- Sumple/app/src/main/java/com/example/sample/second/SampleFragment2.java
+++ Sumple/app/src/main/java/com/example/sample/second/SampleFragment2.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,29 +14,57 @@ import androidx.fragment.app.Fragment;
 
 import com.example.sample.R;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Android 勉強会 第二回
  */
 public class SampleFragment2 extends Fragment {
 
+    //Button押下回数をカウントする変数
+    int n = 0;
+
+    //
+
+    // onCreateViewメソッドで、レイアウトを選択する
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sample, container, false);
+        return inflater.inflate(R.layout.fragment_test, container, false);
     }
 
+    // 処理を記述する
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         // xml 問題
         // 問1 ボタンを表示してみよう。
+        Button button = view.findViewById(R.id.button);
+
+        TextView push_count = view.findViewById(R.id.msgPushButtonCount);
+        push_count.setText(String.valueOf(n));
+
+        ImageView nabeatsu = view.findViewById(R.id.nabeatsu);
+
+
 
         // 問2 文字を表示してみよう。
+        //button.setOnClickListener(v -> {
+        //    //button.setText("TEST");
+        //});
 
         // java 問題
-        // 問1 ボタンを押下してToast(↓)を表示してみよう。
-        Toast.makeText(getContext(), "Android Studio Second Study", Toast.LENGTH_SHORT).show();
-
         // 問2 ボタンを押下した回数を表示してみよう。
+        button.setOnClickListener(v -> {
+            n++;
+            push_count.setText(String.valueOf(n));
+
+            if(n%3 == 0){
+                nabeatsu.setVisibility(View.VISIBLE);
+            }else{
+                nabeatsu.setVisibility(View.INVISIBLE);
+            }
+        });
+
 
     }
 
