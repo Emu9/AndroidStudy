1c082e6b155c6ab63e90f009cf2e0bbd076ac9d2
s_ito <bgjnh540@gmail.com>
2021-06-05 16:24:57 +0900

    伊藤修正

--- Practices/app/src/main/java/com/example/practices/Practice.java
+++ Practices/app/src/main/java/com/example/practices/Practice.java
@@ -6,6 +6,7 @@ import java.util.Calendar;
 
 public class Practice {
 
+  // ok
   public void practice1(int x, int y){
     if(x > y){
       PracticeLog.d("==========================================");
@@ -13,7 +14,7 @@ public class Practice {
       PracticeLog.d("xはyより大きい");
     }
   }
-
+  // ok
   public void practice2(int x, int y){
     if (x > y){
       PracticeLog.d("==========================================");
@@ -27,7 +28,7 @@ public class Practice {
       PracticeLog.d(String.valueOf(y));
     }
   }
-
+  // ok
   public  void  practice3(int x, int y){
     if (x > y){
       PracticeLog.d("==========================================");
@@ -41,7 +42,7 @@ public class Practice {
       PracticeLog.d("xはyより小さい");
     }
   }
-
+  // ok
   public void  practice4(int x, int y){
     if (x > y){
       PracticeLog.d("==========================================");
@@ -62,6 +63,10 @@ public class Practice {
     }
   }
 
+  /**
+   * ok!
+   * 変数の再代入が気になりましたが、明確にダメというわけではないのでこれはありですね！
+   * */
   public  void  practice5(int num){
     String msg = "奇数";
     if (num % 2==0){
@@ -72,6 +77,11 @@ public class Practice {
     PracticeLog.d(msg);
   }
 
+  /**
+   * この書き方....天才ですか....
+   * これは脱帽です。１００点ですね！
+   * 正直この回答は予想していなかったです。しゅごい。。。
+   * */
   public void practice6(int num){
     String plusAndMinus;
     if (num > 0){
@@ -91,6 +101,12 @@ public class Practice {
     PracticeLog.d(String.format(msg, plusAndMinus,evenAndOdd));
   }
 
+  /**
+   * ok!
+   * 変数4パターン定義(不可・優・良・可)して条件一致したら該当する変数をlog出力する、という方式でも良いかもしれません。
+   * 優だった場合、後続の条件分岐は見なくともよいので、アーリーリターンが可能だったりします。
+   * が、これくらいであれば見やすいのでこのままでも全然良いと思います！
+   * */
   public void practice7(){
     int score = 80;
     String msg = "不可";
@@ -108,6 +124,11 @@ public class Practice {
     PracticeLog.d(msg);
   }
 
+  /**
+   * 綺麗な書き方ですね！
+   * アーリーリターンのパターンも記載してみましたが、正直微妙かもです..。
+   * アーリーリターンの弱点として、あとから後続に処理を追加したい場合に困るってのがあるので、必ずしも使うべきというわけではないですね。
+   * */
   public void practice8(){
     int midTermExam = 80;
     int finalExam = 60;
@@ -129,8 +150,42 @@ public class Practice {
     PracticeLog.d("==========================================");
     PracticeLog.d("Practice8 Result");
     PracticeLog.d(msg);
-  }
 
+    // 別解
+    final String NG = "不合格";
+    final String OK = "合格";
+
+    PracticeLog.d("==========================================");
+    PracticeLog.d("Practice8 Result");
+
+    // Todo 本来であればアーリーリターンを使用する場合、条件文を反対にし、NGの場合にリターンを使用するのが良いです。
+    if ((midTermExam >= 60) && (finalExam >= 60)){
+      PracticeLog.d(OK);
+      return;
+    }
+
+    if (midTermExam + finalExam >= 130){
+      PracticeLog.d(OK);
+      return;
+    }
+
+    if (midTermExam + finalExam >= 100){
+      if (midTermExam >= 90){
+        PracticeLog.d(OK);
+        return;
+      }
+      if (finalExam >= 90){
+        PracticeLog.d(OK);
+        return;
+      }
+    }
+
+    PracticeLog.d(NG);
+  }
+  /**
+   * ok!
+   * Java 1.8から登場したYearMonthクラスを使用するとさらにすっきりします！
+   * */
   public  void  practice9(int month){
     if ((month < 0) || (month > 12)){
       PracticeLog.d("==========================================");
@@ -144,8 +199,17 @@ public class Practice {
     PracticeLog.d("==========================================");
     PracticeLog.d("Practice9 Result");
     PracticeLog.d(String.valueOf(calendar.getActualMaximum(Calendar.DAY_OF_MONTH)));
+
+    // https://docs.oracle.com/javase/jp/8/docs/api/java/time/YearMonth.html 参考
+    // その月の日数を返却してくれます。
+    YearMonth yearMonthObject = YearMonth.of(2021, Month);
+    int daysInMonth = yearMonthObject.lengthOfMonth();
+    PracticeLog(String.valueOf(daysInMonth));
   }
 
+  /**
+   * ok
+   * */
   public void practice10(int num){
     String msg = "odd";
     if ((num % 2) ==0){
@@ -156,6 +220,10 @@ public class Practice {
     PracticeLog.d(msg);
   }
 
+  /**
+   * ok!
+   * 引数の数だけ1~10を出力してくれるのですね笑
+   * */
   public void practice11(int cnt){
     PracticeLog.d("==========================================");
     PracticeLog.d("Practice10 Result");
@@ -170,6 +238,9 @@ public class Practice {
     }
   }
 
+  /**
+   * ok!
+   * */
   public void practice12(){
     PracticeLog.d("==========================================");
     PracticeLog.d("Practice11 Result");
