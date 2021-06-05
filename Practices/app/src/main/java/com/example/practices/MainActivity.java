0ee511dd6b8e127e284c1cd82b163120115cbca1
tamasiro <d_tamasiro@i-genius.co.jp>
2021-06-05 16:55:33 +0900

    Java基礎11~20

--- Practices/app/src/main/java/com/example/practices/MainActivity.java
+++ Practices/app/src/main/java/com/example/practices/MainActivity.java
@@ -4,28 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-
-class PracticeLog  {
-    public static void d(String msg) {
-        StackTraceElement calledClass = Thread.currentThread().getStackTrace()[3];
-        Log.d("(" + calledClass.getFileName() + ":"
-                + calledClass.getLineNumber() + ")" , msg);
-    }
-    public static void i(String msg) {
-        StackTraceElement calledClass = Thread.currentThread().getStackTrace()[3];
-        Log.i("(" + calledClass.getFileName() + ":"
-                + calledClass.getLineNumber() + ")" , msg);
-    }
-    public static void e(String msg) {
-        StackTraceElement calledClass = Thread.currentThread().getStackTrace()[3];
-        Log.e("(" + calledClass.getFileName() + ":"
-                + calledClass.getLineNumber() + ")" , msg);
-    }
-}
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "★";
@@ -33,7 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(v -> {
 
+        });
         number1(1,0);
         number2(1,0);
         number3(-1,0);
@@ -44,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
         number8(80,80);
         number9(1-1); //0始まりのためマイナス1
         number10(2);
+        number11();
+        number12();
+        number13(10);
+        number14(6);
+        number15(1,2);
+        number16();
+        number17(2520);
+        number18();
+        number19();
+        number20();
     }
 
     public void number1(int x,int y){
@@ -205,5 +204,118 @@ public class MainActivity extends AppCompatActivity {
         else{
             Log.d(TAG,"odd");
         }
+    }
+
+    public void number11(){
+        for(int i = 1; i < 11; i++){
+            Log.d(TAG, String.valueOf(i));
+        }
+    }
+
+    public void number12(){
+        for(int x = 1; x < 10; x++){
+            for(int y = 1; y < 10; y++){
+                Log.d(TAG, String.valueOf(x*y));
+            }
+        }
+    }
+
+    public void number13(int x){
+        if((x % 10 == 0 && x != 10) || x <= 0){
+            Log.d(TAG,"xは一桁の自然数");
+        }
+        else{
+            Log.d(TAG,"xは一桁の自然数でない");
+        }
+    }
+
+    public void number14(int visitors){
+        int fee = 600;
+        if(visitors >= 20){
+            fee = 500;
+        }
+        else if(visitors >= 5){
+            fee = 550;
+        }
+        int sum = fee * visitors;
+        Log.d(TAG, String.valueOf(sum));
+    }
+
+    public void number15(int x, int y){
+        Log.d(TAG,"入れ替え前の順番は" + x + "," + y);
+        int a = x;
+        x = y;
+        y = a;
+        Log.d(TAG,"入れ替え後の順番は" + x + "," + y);
+    }
+
+    public void number16(){
+        for(int i = 1; i < 101; i++){
+            if(i % 3 ==0 && i % 5 ==0){
+                Log.d(TAG,"foobar");
+            }
+            else if(i % 3 == 0){
+                Log.d(TAG, "foo");
+            }
+            else if(i % 5 == 0){
+                Log.d(TAG,"bar");
+            }
+            else{
+                Log.d(TAG, String.valueOf(i));
+            }
+        }
+    }
+
+    public void number17(int x){
+        String result = "";
+        while(true){
+            if(x < 2){
+                Log.d(TAG, result);
+                break;
+            }
+            for(int i = 2; i <= x;){
+                if(x % i == 0){
+                    result += String.valueOf(i);
+                    if(x != i){
+                        result += " * ";
+                    }
+                    x /= i;
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+    }
+
+    public void number18(){
+        ArrayList<String> lists = new ArrayList<>();
+        lists.add("ハンバーガー");
+        lists.add("チーズバーガー");
+        lists.add("ダブルチーズバーガー");
+        for (String list: lists){
+            Log.d(TAG, list);
+        }
+    }
+
+    public void number19(){
+        //HashMapインスタンスを生成
+        Map<String, Integer> map = new HashMap<>();
+        //各科目の科目名と得点を要素として追加
+        map.put("国語",90);
+        map.put("数学",80);
+        map.put("英語",70);
+        for(Map.Entry<String, Integer> entry: map.entrySet()){
+            Log.d(TAG, "Key:" + entry.getKey() + " Value:" + entry.getValue());
+        }
+    }
+
+    static public void number20() {
+        List<String> helloWorld = new ArrayList<String>(3);
+        helloWorld.add("Hello");
+        helloWorld.add(" ");
+        helloWorld.add("World");
+        helloWorld.add("!");
+        helloWorld.forEach(s -> Log.d(TAG, s));
     }
 }
