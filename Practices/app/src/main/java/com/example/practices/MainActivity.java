3e5d350be0a65c196003ab1a6083d64af88ef193
M Okamoto <masaomasao99@gmail.com>
2021-06-05 16:34:18 +0900

    第６回勉強会 練習問題集(11～20)

--- Practices/app/src/main/java/com/example/practices/MainActivity.java
+++ Practices/app/src/main/java/com/example/practices/MainActivity.java
@@ -7,8 +7,12 @@ import android.util.Log;
 import android.view.Window;
 
 import java.time.YearMonth;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,25 +28,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        compareNumber(1,2);
 //        compareNumber(2,1);
+
 //        compareNumber2(1,2);
 //        compareNumber2(2,1);
+
 //        compareNumber3(1,2);
 //        compareNumber3(2,1);
+
 //        compareNumber4(1,2);
 //        compareNumber4(2,1);
 //        compareNumber4(2,2);
+
 //        judgmentNumber(1);
 //        judgmentNumber(2);
 //        judgmentNumber2(-1);
 //        judgmentNumber2(-2);
 //        judgmentNumber2(1);
 //        judgmentNumber2(2);
+
 //        judgmentTestScore(80);
 //        judgmentTestScore(79);
 //        judgmentTestScore(70);
 //        judgmentTestScore(69);
 //        judgmentTestScore(60);
 //        judgmentTestScore(59);
+
 //        judgmentTestScore2(60,60);
 //        judgmentTestScore2(60,59);
 //        judgmentTestScore2(50,80);
@@ -53,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
 //        judgmentTestScore2(90,10);
 //        judgmentTestScore2(9,90);
 //        judgmentTestScore2(90,9);
+
 //        showMonthDays(0);
 //        showMonthDays(1);
 //        showMonthDays(2);
@@ -67,10 +78,35 @@ public class MainActivity extends AppCompatActivity {
 //        showMonthDays(11);
 //        showMonthDays(12);
 //        showMonthDays(13);
+//
+//        judgmentNumber3(0);
+//        judgmentNumber3(1);
+//        judgmentNumber3(2);
 
-        judgmentNumber3(0);
-        judgmentNumber3(1);
-        judgmentNumber3(2);
+//        multiplication();
+
+//        multiplication();
+//        multiplication();
+//        isValueIsOneDigits(0);
+//        isValueIsOneDigits(1);
+//        isValueIsOneDigits(9);
+//        isValueIsOneDigits(10);
+
+//        int galleryEntranceCharge = galleryEntranceChargeCalculate(4);
+//        Log.d(TAG, String.valueOf(galleryEntranceCharge));
+//        galleryEntranceCharge = galleryEntranceChargeCalculate(5);
+//        Log.d(TAG, String.valueOf(galleryEntranceCharge));
+//        galleryEntranceCharge = galleryEntranceChargeCalculate(19);
+//        Log.d(TAG, String.valueOf(galleryEntranceCharge));
+//        galleryEntranceCharge = galleryEntranceChargeCalculate(20);
+//        Log.d(TAG, String.valueOf(galleryEntranceCharge));
+
+//        frontRearReplacingVariableValue(1,2);
+//        UpToNumberRecurringOutFizzBuzz();
+//        inputNumPrimeFactorization(100);
+//        outputLogArrayList();
+//        outputLogHashMap();
+        practice20();
     }
 
     // ok!! javadocも記載されててかなり良きです。
@@ -221,7 +257,7 @@ public class MainActivity extends AppCompatActivity {
      * @param finalScore
      */
     public void judgmentTestScore2(int midScore, int finalScore){
-        String passing = "合格";
+        final String passing = "合格";
         if(midScore >= 60 && finalScore >= 60){
             Log.d(TAG,passing);
         } else if(midScore + finalScore >= 130){
@@ -282,4 +318,188 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG,"odd");
         }
     }
+
+
+    /**
+     * 11	1~10までの数字を繰り返し出力してみよう。
+     */
+    public void UpToNumberRecurringOut(){
+        for(int i = 1; i <= 10; i++){
+            Log.d(TAG, String.valueOf(i));
+        }
+    }
+
+    /**
+     * 12	九九の結果を出力してみよう。1*1 ~ 9*9
+     */
+
+
+    public void multiplication(){
+        for(int k = 1; k <= 9; k++){
+            Log.d(TAG, "------------------------------");
+            Log.d(TAG, k + "の段");
+            for(int t = 1; t <= 9; t++){
+                Log.d(TAG, "------------------------------");
+                Log.d(TAG, String.valueOf(k * t));
+            }
+        }
+    }
+
+    /**
+     * 13	整数値を入力させ、その値が一桁の自然数かそうでないか判定するプログラムを作成せよ。
+     * @param i
+     */
+    public void isValueIsOneDigits(int i){
+        if(i > 0 && i <= 10){
+            Log.d(TAG, String.valueOf(i) + "は一桁の自然数です");
+        }
+        else{
+            Log.d(TAG, String.valueOf(i) + "は一桁の自然数ではありません");
+        }
+    }
+
+    /**
+     * 14	美術館の入場料金は、一人600円であるが、5人以上のグループなら一人550円、20人以上の団体なら一人500円である。人数を入力し、入場料の合計を計算するプログラムを作成せよ。
+     * @param count 美術館の総入場人数
+     * @return 美術館の総入場料金
+     */
+    public int galleryEntranceChargeCalculate(int count){
+        if(count >= 20){
+            return count * 500;
+        }
+        else if (count >= 5){
+            return count * 550;
+        }
+        else {
+            return count * 600;
+        }
+    }
+
+    /**
+     * 15	"異なる整数値を2つ入力し、それぞれ別の変数に格納する。
+     * そして、それらの変数の値を入れ替える前、入れ替えた後で出力する。"
+     */
+    public void frontRearReplacingVariableValue(int x,int y){
+        int a = x;
+        int b = y;
+        Log.d(TAG, "入れ替え前");
+        Log.d(TAG, String.valueOf(a));
+        Log.d(TAG, String.valueOf(b));
+        a = y;
+        b = x;
+        Log.d(TAG, "入れ替え後");
+        Log.d(TAG, String.valueOf(a));
+        Log.d(TAG, String.valueOf(b));
+
+    }
+
+
+    /**
+     * 16	1から100までの値を繰り返しで表示するが、
+     * 3の倍数の時はfoo、5の倍数の時はbarと数字の代わりに表示するプログラムを作成せよ。
+     * なお、3と5の両方の倍数の時はfoobarと表示される。
+     */
+    public void UpToNumberRecurringOutFizzBuzz(){
+        String foo = "foo";
+        String bar = "bar";
+        String foobar = "foo" + "bar";
+        for(int i = 1; i <= 100; i++){
+            if(i % 3 == 0 && i % 5 == 0) {
+                Log.d(TAG, foobar);
+            }
+            else if(i % 3 == 0) {
+                Log.d(TAG, foo);
+            }
+            else if(i % 5 == 0) {
+                Log.d(TAG, bar);
+            }
+            else{
+                Log.d(TAG, String.valueOf(i));
+            }
+        }
+    }
+
+    /**
+     * 17	自然数の入力値を素因数分解して結果を表示するプログラムを作成せよ。
+     * @param num 整数
+     */
+    public void inputNumPrimeFactorization(int num){
+        Log.d(TAG, num + "=");
+        for(int i = 2;i<=num;i++){
+            if(num % i == 0){
+                num /= i;
+                if(num == 1) Log.d(TAG, String.valueOf(i));
+                else Log.d(TAG, String.valueOf(i) + "✕");
+                i = 1;
+            }
+        }
+    }
+
+    /**
+     * 18	"以下のArrayListを拡張for文でログに出力しよう。
+     *
+     *       ArrayList<String> list = new ArrayList<>();
+     *         list.add(""ハンバーガー"");
+     *         list.add(""チーズバーガー"");
+     *         list.add(""ダブルチーズバーガー"");"
+     */
+    public void outputLogArrayList(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("ハンバーガー");
+        list.add("チーズバーガー");
+        list.add("ダブルチーズバーガー");
+
+        for(String item:list){
+            Log.d(TAG, item);
+        }
+    }
+
+    /**
+     * 19	"以下のHashMapを拡張for分でログに出力しよう。
+     *
+     *         //HashMapインスタンスを生成
+     *         Map<String, Integer> map = new HashMap<>();
+     *         //各科目の科目名と得点を要素として追加
+     *         map.put(""国語"",90);
+     *         map.put(""数学"",80);
+     *         map.put(""英語"",70);"
+     */
+    public void outputLogHashMap(){
+        //HashMapインスタンスを生成
+        Map<String, Integer> map = new HashMap<>();
+        //各科目の科目名と得点を要素として追加
+        map.put("国語",90);
+        map.put("数学",80);
+        map.put("英語",70);
+
+
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            Log.d(TAG, entry.getKey() + " : " + entry.getValue());
+        }
+    }
+
+    /**
+     * 20  forループをStream APIのforEachメソッドで置き換えよ。[+α] コードをより短くなるように改良せよ。
+     *   static public void practice10() {
+     *     List<String> helloWorld = new ArrayList<String>(3);
+     *     helloWorld.add("Hello");
+     *     helloWorld.add(" ");
+     *     helloWorld.add("World");
+     *     helloWorld.add("!");
+     *     for (int i = 0; i < helloWorld.size(); i++) {
+     *       System.out.print(helloWorld.get(i));
+     *     }
+     *   }
+     */
+
+    static public void practice20() {
+            List<String> helloWorld = new ArrayList<>();
+            helloWorld.add("Hello");
+            helloWorld.add(" ");
+            helloWorld.add("World");
+            helloWorld.add("!");
+            for (int i = 0; i < helloWorld.size(); i++) {
+                Log.d(TAG,helloWorld.get(i));
+            }
+        }
 }
